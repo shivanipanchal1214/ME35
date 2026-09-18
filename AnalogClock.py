3d254263fb3a7957c6e021c50d962692a8648710
shivanipanchal1214 <shivani.panchal@tufts.edu>
2026-09-18 14:09:13 -0400

    Update AnalogClock.py

--- AnalogClock.py
+++ AnalogClock.py
@@ -2,25 +2,34 @@ from machine import RTC, PWM, Pin
 from time import sleep
 
 rtc = RTC()
-rtc.datetime((2026,9,16,0,12,59,58,0))
+rtc.datetime((2026,9,16,0,13,59,55,0))
 print(rtc.datetime())
 
-# while True:
-#     current = rtc.datetime()
-#     hour = current[4]
-#     minute = current[5]
-#     second = current[6]
-#     if hour > 12:
-#         hour = hour - 12
-#     print(hour,minute,second)
-# sleep(1)
-    
 servo = PWM(Pin(4), freq=50, duty_u16=0)
 
-hour=1
-angle = hour*15
-for i in range(13):
-    pulse = 2500 - (((2*angle)/180)*1000)
-    servo.duty_ns(int(pulse*1000))
-    angle = angle + 15
+while True:
+    current = rtc.datetime()
+    hour = current[4]
+    minute = current[5]
+    second = current[6]
+    if hour >= 12:
+        hour = hour%12
+    print(hour,minute,second)
+
+#     angle_hour = hour*15
+# #     angle = max(0, min(180, angle))
+#     pulse_hour = 2500 - (((2*angle_hour)/180)*1000)
+#     servo.duty_ns(int(pulse_hour*1000))
+#     sleep(1)
+    
+    if minute <= 30:
+        angle_min = minute*6
+    else:
+        angle_min = 180 - (minute - 30)*6
+    pulse_min = 2500 - (((2*angle_min)/180)*1000)
+    servo.duty_ns(int(pulse_min*1000))
     sleep(1)
+    
+    
+    
+
