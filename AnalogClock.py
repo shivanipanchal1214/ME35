04d4210e7118a5104d8f81f4da3e274a64e13cd6
shivanipanchal1214 <shivani.panchal@tufts.edu>
2026-09-20 21:46:09 -0400

    edits for project 1

--- AnalogClock.py
+++ AnalogClock.py
@@ -1,15 +1,43 @@
 from machine import RTC, PWM, Pin
-from time import sleep
+import time
+import urequests, secretsclock
+import gc
 
-rtc = RTC()
-rtc.datetime((2026,9,16,0,3,59,55,0))
-print(rtc.datetime())
-
+#initial servo and LED pins
 servo1 = PWM(Pin(4), freq=50, duty_u16=0)
 servo2 = PWM(Pin(18), freq=50, duty_u16=0)
+led_warm = Pin(13,Pin.OUT)
+led_mild = Pin(2,Pin.OUT)
+led_warm.off()
+led_mild.off()
+
+#wifi connecting for API key
+def wifi_connect():
+    import network
+    sta_if = network.WLAN(network.WLAN.IF_STA)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(secretsclock.SSID, secretsclock.PWD)
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ipconfig('addr4'))  
+wifi_connect()
+url =f"https://api.openweathermap.org/data/2.5/weather?q=Queens,US-NY,840&appid={secretsclock.weather_api}"
+
+
+#Part1
+rtc = RTC()
+rtc.datetime((2026,9,16,0,17,59,55,0))
+print(rtc.datetime())
 
 angle_hour = 0
 angle_min = 0
+
+first_check = False
+weather_interval = 10 * 1000   # 10 seconds to start
+weather_interval_normal = 60 * 1000 #60 second intervals
+last_weather_check = time.ticks_ms()
 
 while True:
     current = rtc.datetime()
@@ -17,7 +45,7 @@ while True:
     minute = current[5]
     second = current[6]
     if hour == 0:
-        hour == 12
+        hour = 12
         print(hour, minute, second, "AM")
     elif hour == 12:
         print(hour, minute, second, "PM")
@@ -41,64 +69,45 @@ while True:
     servo2.duty_ns(int(pulse_min*1000))
     if minute == 0:
         angle_min = 0
-    sleep(1)
     
-    
-#     if minute <= 30:
-#         angle_min = minute*6
-#     else:
-#         angle_min = 180 - (minute - 30)*6
-#     pulse_min = 2500 - (((2*angle_min)/180)*1000)
-#     servo2.duty_ns(int(pulse_min*1000))
-#     sleep(1)
+#    API key Part 2    
+    if time.ticks_diff(time.ticks_ms(),last_weather_check) > weather_interval:
+        #print(gc.mem_free())
+        try:
+            response = urequests.get(url)
+            #print(response.status_code)
+            data = response.json()
+            #print(data)
+            response.close()
+            gc.collect()
+            #print(gc.mem_free())
+            temp_kelvin = data['main']['temp']
+            temp = (temp_kelvin - 273.15) * 1.8 + 32
+            #print("Temp:", temp)
+            
+            #LED
+            temp_high = 80
+            temp_med = 70
+            temp_low = 60
 
+            if temp_med <= temp <= temp_high:
+                led_warm.on()
+                print("Weather is warm:", temp, "degrees Fahrenheit")
+            elif temp_low <= temp < temp_med:
+                led_mild.on()
+                print("Weather is mild:", temp, "degrees Fahrenheit")
+                
+        except Exception as e:
+            print("Updated weather check failed:", e)
+            #print(gc.mem_free())
 
-#API key Part 2
-from Day3 import urequests, secretsclock
+        last_weather_check = time.ticks_ms()
 
-def wifi_connect():
-    import network
-    
-    sta_if = network.WLAN(network.WLAN.IF_STA)
-    if not sta_if.isconnected():
-        print('connecting to network...')
-        sta_if.active(True)
-        sta_if.connect(secretsclock.SSID, secretsclock.PWD)
-        while not sta_if.isconnected():
-            pass
-    print('network config:', sta_if.ipconfig('addr4'))
-    
-wifi_connect()
-
-url =f"https://api.openweathermap.org/data/2.5/forecast?q=Queens,US-NY,840&appid={secretsclock.weather_api}"
-
-response = urequests.get(url)
-
-print(response.status_code)
-if response.status code == 200:
-    data = response.json()
-    print(data)
-    
-#### 
-response.close()             # free the socket
-
-temp = data['main']['temp']
-#condition = data['weather'][0]['main']       # e.g. "Rain", "Clear", "Clouds"
-temp_cel = temp - 273.15
-
-print("Temp:", temp)
-print("Condition:", condition)
-
-led = LED(17) #check pin
-temp_high = 80
-temp_med = 60
-
-
-#LED
-from LED import gpio
-if temp_med < temp < temp_high:
-    led.on()
-    print("Condition met: LED is on")
+        if not first_check:
+            weather_interval = weather_interval_normal
+            first_check = True
+        
+    time.sleep(1)
 
 
 
